3e6466db080719a95f31815e75b5496384217771
qkdflrgs <qkdflrgs@gmail.com>
2025-04-18 16:20:49 +0900

    modify post

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(express.json());
 app.post("/webhook", (req, res) => {
     console.log("Received webhook:", req.body);
     if (!req.body || Object.keys(req.body).length === 0) {
+        console.log('fail')
         return res.status(400).json({ success: false, message: "Empty body" });
     }
+    console.log('success')
     return res.status(200).json({ success: true, message: "Webhook received" });
 });
 
