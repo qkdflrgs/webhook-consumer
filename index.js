369920cbc8eaf0cec89ddb9d198c7b89fe8189c4
qkdflrgs <qkdflrgs@gmail.com>
2025-04-18 12:51:26 +0900

    update

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.use(express.json());
 
 app.post("/webhook", (req, res) => {
     console.log("Received webhook:", req.body);
-    res.status(200).send("Webhook received");
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).json({ success: false, message: "Empty body" });
+    }
+    res.status(200).json({ success: true, message: "Webhook received" });
 });
 
 app.listen(PORT, () => {
